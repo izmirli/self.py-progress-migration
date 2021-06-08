ffe4541ffa972f4557c16d1d6ca67d58e13c4bcd
Oren Izmirli <dev@izmirli.org>
2021-06-08 21:26:20 +0300

    Adding README and some code refactoring

--- sppm.py
+++ sppm.py
@@ -1,4 +1,4 @@
-"""Self.Py Progress Migration (SPPM).
+"""Self.Py Progress Migration (SPPM)
 
 The self.py course has a half-year cycle (summer/winter). At the end of
 a cycle, the old course will be archived (viewable, but no longer
@@ -17,7 +17,7 @@ Before using do/check these:
     `pip install -U selenium`
 
     2. Install WebDriver for Chrome from:
-    https://sites.google.com/a/chromium.org/chromedriver/downloads
+    https://sites.google.com/chromium.org/driver/downloads
     And add its location to system's path. Read more about it here:
     https://www.selenium.dev/documentation/en/webdriver/driver_requirements/
 
@@ -25,19 +25,50 @@ Before using do/check these:
     at same directory as this file.
     You can copy (or move) conf.ini.sample to conf.ini and edit it.
 """
+import logging
+import os
+from configparser import ConfigParser
+
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
-from configparser import ConfigParser
-import logging
+from selenium.webdriver.support.ui import WebDriverWait
+
+__version__ = "1.1"
 
 BASE_URL = 'https://courses.campus.gov.il/'
 PAGE_LOAD_WAIT = 3
 LONG_WAIT = 10
+LOG_LEVEL = logging.INFO
 
 config = {}
 driver: Chrome
+total_exs: int
+done_exs = 0
+
+
+def main():
+    initialize()
+    campus_il_login()
+    migrate_progression()
+    driver.quit()
+
+
+def initialize():
+    """Initialize this script.
+
+    1. Set logging config.
+    2. Read and set this script's config.
+    3. Initialize Selenium's Chrome webdriver.
+    4. Maximize Chrome window.
+    """
+    global driver
+    logging.basicConfig(
+        format='[%(asctime)s] [%(levelname)s] %(message)s', level=LOG_LEVEL
+    )
+    read_config()
+    driver = Chrome()
+    driver.maximize_window()
 
 
 def campus_il_login():
@@ -48,51 +79,31 @@ def campus_il_login():
     password_in.send_keys(config['pass'])
     password_in.send_keys(Keys.RETURN)
     logging.debug('Login should have been submitted')
-    WebDriverWait(driver, LONG_WAIT).until(lambda d: 'לוח בקרה' in d.title)
+    WebDriverWait(driver, PAGE_LOAD_WAIT).until(lambda d: 'לוח בקרה' in d.title)
     logging.debug(f'login done? title is: {driver.title}')
     logging.info('Logged in to Campus IL')
 
 
-def goto_exercise(cycle: str, exercise: str):
-    """Go to given exercise page of given cycle.
-
-    :param cycle: course cycle url-string (e.g. 2021_2).
-    :param exercise: exercise name (e.g. "1.3.1").
-    :return: None
-    """
-    chapter = get_chapter(exercise)
-    driver.get(
-        f'{BASE_URL}courses/course-v1:CS+GOV_CS_selfpy101+{cycle}/progress'
-    )
-    WebDriverWait(driver, LONG_WAIT).until(lambda d: 'התקדמות' in d.title)
-    logging.debug(f'At {cycle}/progress? page_title is: {driver.title}')
-
-    chapter_link = driver.find_element_by_xpath(
-        f'//a[contains(text(), "{chapter}")]'
-    )
-    chapter_link.click()
-    logging.debug(f'got to chapter {chapter}? page_title is: {driver.title}')
-
-    exercise_but = driver.find_element_by_xpath(
-        f'//button[@data-page-title="תרגיל {exercise}"]'
-    )
-    exercise_but.click()
-    logging.debug(f'got to ex {exercise}? page_title is: {driver.title}')
-
-
-def submit_answers(exercise: str):
-    """Fine all submit buttons and click them (unless disabled).
-
-    :param exercise: exercise name (e.g. "1.3.1").
-    :return: None
-    """
-    submit_buttons = driver.find_elements_by_xpath(
-        '//div[@class="problem"]//button[@data-value="הגשה" '
-        'and not(@disabled="disabled")]'
-    )
-    for i, but in enumerate(submit_buttons):
-        but.click()
-        logging.info(f'Submitted Ex {exercise} (sub#{i})')
+def migrate_progression():
+    """Traverse relevant exercises, get old cycle answers and submit to new."""
+    global done_exs
+    # config['exercises'] is a dict with exercise lists as values for each type.
+    for exercise_type in config['exercises']:
+        for exercise in config['exercises'][exercise_type]:
+            logging.debug(f'migrate_progression of {exercise} '
+                          f'(type: {exercise_type})')
+            if len(config['last_exercise']) >= 5 \
+                    and exercise > config['last_exercise']:
+                logging.debug(f'Skipping Ex#{exercise} as after last exercise '
+                              f'to import ({config["last_exercise"]})')
+                continue
+            if exercise_type == 'text':
+                import_text_answers(exercise)
+            elif exercise_type == 'select':
+                import_select_answers(exercise)
+            elif exercise_type in ('radio', 'checkbox'):
+                import_clicked_answers(exercise, exercise_type)
+            done_exs += 1
 
 
 def import_text_answers(exercise: str):
@@ -165,30 +176,50 @@ def import_clicked_answers(exercise: str, input_type: str):
     submit_answers(exercise)
 
 
-def migrate_progression():
-    """Traverse relevant exercises, get old cycle answers and submit to new."""
-    exercises = {
-        'text': ["1.3.1", "3.1.1", "3.3.1", "3.3.2", "3.4.1", "3.4.4", "4.2.1"],
-        'select': ["2.1.1", "2.2.1", "2.3.2", "4.1.1", "5.2.1", "5.3.3",
-                   "6.1.1", "6.2.1", "7.1.3", "8.3.1"],
-        'radio': ["2.1.2", "2.3.1", "2.4.1", "7.1.1", "7.1.2", "8.1.1", "9.2.1"],
-        'checkbox': ["5.1.1", "5.3.1", "5.3.2", "6.2.2", "7.2.3"],
-    }
-    for exercise_type in exercises:
-        for exercise in exercises[exercise_type]:
-            logging.debug(f'migrate_progression of {exercise} '
-                          f'(type: {exercise_type})')
-            if len(config['last_exercise']) >= 5 \
-                    and exercise > config['last_exercise']:
-                logging.debug(f'Skipping Ex#{exercise} as after last exercise '
-                              f'to import ({config["last_exercise"]})')
-                continue
-            if exercise_type == 'text':
-                import_text_answers(exercise)
-            elif exercise_type == 'select':
-                import_select_answers(exercise)
-            elif exercise_type in ('radio', 'checkbox'):
-                import_clicked_answers(exercise, exercise_type)
+def goto_exercise(cycle: str, exercise: str):
+    """Go to given exercise page of given cycle.
+
+    :param cycle: course cycle url-string (e.g. 2021_2).
+    :param exercise: exercise name (e.g. "1.3.1").
+    :return: None
+    """
+    chapter = get_chapter(exercise)
+    driver.get(
+        f'{BASE_URL}courses/{config["course_url_str"]}+{cycle}/progress'
+    )
+    WebDriverWait(driver, PAGE_LOAD_WAIT).until(lambda d: 'התקדמות' in d.title)
+    logging.debug(f'At {cycle}/progress? page_title is: {driver.title}')
+
+    chapter_link = driver.find_element_by_xpath(
+        f'//a[contains(text(), "{chapter}")]'
+    )
+    chapter_link.click()
+    logging.debug(f'got to chapter {chapter}? page_title is: {driver.title}')
+
+    exercise_but = driver.find_element_by_xpath(
+        f'//button[@data-page-title="תרגיל {exercise}"]'
+    )
+    exercise_but.click()
+    logging.debug(f'got to ex {exercise}? page_title is: {driver.title}')
+
+
+def submit_answers(exercise: str):
+    """Fine all submit buttons and click them (unless disabled).
+
+    :param exercise: exercise name (e.g. "1.3.1").
+    :return: None
+    """
+    submit_buttons = driver.find_elements_by_xpath(
+        '//div[@class="problem"]//button[@data-value="הגשה" '
+        'and not(@disabled="disabled")]'
+    )
+    for i, but in enumerate(submit_buttons):
+        but.click()
+        logging.info(f'Submitted Ex {exercise} (sub#{i}) '
+                     f'[{done_exs + 1}/{total_exs}]')
+    if len(submit_buttons) == 0:
+        logging.info(f'Nothing submitted at Ex {exercise} '
+                     f'[{done_exs + 1}/{total_exs}]')
 
 
 def get_chapter(exercise: str) -> str:
@@ -205,42 +236,41 @@ def read_config():
 
     Mast have all expected values, or script will exit.
     """
-    global config
+    global config, total_exs
+    conf_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), 'conf.ini'
+    )
+    if not os.path.isfile(conf_path):
+        logging.fatal(f'Configuration file (conf.ini) is missing '
+                      f'(should be in same directory as sppm.py).')
+        raise SystemExit
     cp = ConfigParser()
     try:
-        cp.read('./conf.ini')
+        cp.read(conf_path)
         config['email'] = cp['LOGIN']['EMAIL']
         config['pass'] = cp['LOGIN']['PASSWD']
+        config['course_url_str'] = cp['CYCLES']['COURSE']
         config['old_cycle'] = cp['CYCLES']['OLD']
         config['new_cycle'] = cp['CYCLES']['NEW']
         config['last_exercise'] = cp['EXERCISES']['LAST']
+        config['exercises'] = {
+            ex_type.lower(): [
+                cur_ex for cur_ex in cp['EXERCISES'][ex_type].split(', ')
+            ]
+            for ex_type in ('TEXT', 'SELECT', 'RADIO', 'CHECKBOX')
+        }
+        total_exs = sum([
+            len([
+                li for li in cur_list
+                if len(config['last_exercise']) < 5 or li <= config[
+                    'last_exercise']
+            ]) for cur_list in config['exercises'].values()
+        ])
+        logging.info(f'Got {total_exs} exercise pages to migrate.')
+
     except Exception as ex:
         logging.fatal(f'Missing or invalid conf.ini data - {ex.__repr__()}')
         raise SystemExit
-
-
-def initialize():
-    """Initialize this script.
-
-    1. Set logging config.
-    2. Read and set this script's config.
-    3. Initialize Selenium's Chrome webdriver.
-    4. Maximize Chrome window.
-    """
-    global driver
-    logging.basicConfig(
-        format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.INFO
-    )
-    read_config()
-    driver = Chrome()
-    driver.maximize_window()
-
-
-def main():
-    initialize()
-    campus_il_login()
-    migrate_progression()
-    driver.quit()
 
 
 if __name__ == '__main__':
